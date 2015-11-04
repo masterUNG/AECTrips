bffde0b39a61e1cdda3df65b2f88df4ac8876d93
masterUNG <phrombutr@gmail.com>
2015-11-04 11:46:36 +0700

    สร้างฐานข้อมูล SQLite เสร็จแว้วววว

--- app/src/main/java/appewtc/masterung/aectrips/CountryTABLE.java
+++ app/src/main/java/appewtc/masterung/aectrips/CountryTABLE.java
@@ -1,11 +1,24 @@
 package appewtc.masterung.aectrips;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
 /**
  * Created by masterUNG on 10/14/15 AD.
  */
 public class CountryTABLE {
 
-    public CountryTABLE() {
+    //Explicit
+    private MyOpenHelper objMyOpenHelper;
+    private SQLiteDatabase writeSqLiteDatabase, readSqLiteDatabase;
+
+    public CountryTABLE(Context context) {
+
+        //Create & Connected Database
+        objMyOpenHelper = new MyOpenHelper(context);
+        writeSqLiteDatabase = objMyOpenHelper.getWritableDatabase();
+        readSqLiteDatabase = objMyOpenHelper.getReadableDatabase();
+
     }   // Constructor
 
 }   // Main Class
--- app/src/main/java/appewtc/masterung/aectrips/MainActivity.java
+++ app/src/main/java/appewtc/masterung/aectrips/MainActivity.java
@@ -5,9 +5,17 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Explicit
+    private CountryTABLE objCountryTABLE;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //Create & Connected Database
+        objCountryTABLE = new CountryTABLE(this);
+
+    }   // Main Method
+
+}   // Main Class
