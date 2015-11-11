027adda7204f31a8a66432a197525f25bf24cd75
masterUNG <phrombutr@gmail.com>
2015-11-11 09:48:25 +0700

    Start 11Nov15

--- app/src/main/java/appewtc/masterung/aectrips/CountryTABLE.java
+++ app/src/main/java/appewtc/masterung/aectrips/CountryTABLE.java
@@ -1,5 +1,6 @@
 package appewtc.masterung.aectrips;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
@@ -12,6 +13,16 @@ public class CountryTABLE {
     private MyOpenHelper objMyOpenHelper;
     private SQLiteDatabase writeSqLiteDatabase, readSqLiteDatabase;
 
+    public static final String COUNTRY_TABLE = "countryTABLE";
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_COUNTRY = "Country";
+    public static final String COLUMN_NAME = "Name";
+    public static final String COLUMN_DESCRIPTION = "Description";
+    public static final String COLUMN_LAT = "Lat";
+    public static final String COLUMN_LNG = "Lng";
+    public static final String COLUMN_PICTURE = "Picture";
+
+
     public CountryTABLE(Context context) {
 
         //Create & Connected Database
@@ -20,5 +31,20 @@ public class CountryTABLE {
         readSqLiteDatabase = objMyOpenHelper.getReadableDatabase();
 
     }   // Constructor
+
+    //Add New Value to countryTABLE
+    public long addNewValue(String strCountry, String strName, String strDescription, String strLat, String strLng, String strPicture) {
+
+        ContentValues objContentValues = new ContentValues();
+        objContentValues.put(COLUMN_COUNTRY, strCountry);
+        objContentValues.put(COLUMN_NAME, strName);
+        objContentValues.put(COLUMN_DESCRIPTION, strDescription);
+        objContentValues.put(COLUMN_LAT, strLat);
+        objContentValues.put(COLUMN_LNG, strLng);
+        objContentValues.put(COLUMN_PICTURE, strPicture);
+
+        return writeSqLiteDatabase.insert(COUNTRY_TABLE, null, objContentValues);
+    }
+
 
 }   // Main Class
--- app/src/main/java/appewtc/masterung/aectrips/MainActivity.java
+++ app/src/main/java/appewtc/masterung/aectrips/MainActivity.java
@@ -1,7 +1,22 @@
 package appewtc.masterung.aectrips;
 
+import android.database.sqlite.SQLiteDatabase;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +31,90 @@ public class MainActivity extends AppCompatActivity {
         //Create & Connected Database
         objCountryTABLE = new CountryTABLE(this);
 
+        //Tester Add Value
+        //testerAddValue();
+
+        //Delete All Data
+        deleteAllData();
+
+        //Synchronize JSON SQLite
+        synJSONtoSQLite();
+
     }   // Main Method
+
+    private void synJSONtoSQLite() {
+
+        //SetUp New Policy
+        StrictMode.ThreadPolicy newPolicy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(newPolicy);
+
+        //1. Create InputStream
+        InputStream objInputStream = null;
+        String strJSON = null;
+
+        try {
+
+            HttpClient objHttpClient = new DefaultHttpClient();
+            HttpPost objHttpPost = new HttpPost("http://swiftcodingthai.com/aec/php_get_data_country.php");
+            HttpResponse objHttpResponse = objHttpClient.execute(objHttpPost);
+            HttpEntity objHttpEntity = objHttpResponse.getEntity();
+            objInputStream = objHttpEntity.getContent();
+
+        } catch (Exception e) {
+            Log.d("aec", "InputStream ==> " + e.toString());
+        }
+
+        //2. Create JSON String
+        try {
+
+            BufferedReader objBufferedReader = new BufferedReader(new InputStreamReader(objInputStream, "UTF-8"));
+            StringBuilder objStringBuilder = new StringBuilder();
+            String strLine = null;
+
+            while ((strLine = objBufferedReader.readLine()) != null ) {
+                objStringBuilder.append(strLine);
+            }   // while
+
+            objInputStream.close();
+            strJSON = objStringBuilder.toString();
+
+        } catch (Exception e) {
+            Log.d("aec", "JSON String ==> " + e.toString());
+        }
+
+        //3. Update to SQLite
+        try {
+
+            JSONArray objJsonArray = new JSONArray(strJSON);
+
+            for (int i = 0; i < objJsonArray.length(); i++) {
+
+                JSONObject object = objJsonArray.getJSONObject(i);
+                String strCountry = object.getString("Country");
+                String strName = object.getString("Name");
+                String strDescription = object.getString("Description");
+                String strLat = object.getString("Lat");
+                String strLng = object.getString("Lng");
+                String strPicture = object.getString("Picture");
+                objCountryTABLE.addNewValue(strCountry, strName, strDescription, strLat, strLng, strPicture);
+
+            }   // for
+
+        } catch (Exception e) {
+            Log.d("aec", "Update ==> " + e.toString());
+        }
+
+
+
+    }   // synJSONtoSQLite
+
+    private void deleteAllData() {
+        SQLiteDatabase objSqLiteDatabase = openOrCreateDatabase("AEC.db", MODE_PRIVATE, null);
+        objSqLiteDatabase.delete("countryTABLE", null, null);
+    }
+
+    private void testerAddValue() {
+        objCountryTABLE.addNewValue("testCountry", "testName", "testDescription", "testLat", "testLng", "testPicture");
+    }
 
 }   // Main Class
