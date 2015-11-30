23745f8375aba5fe0889da27fb410f27228a73a3
masterUNG <phrombutr@gmail.com>
2015-11-30 14:31:41 +0700

    Marker ใกล้สำเร็จละ

--- app/src/main/java/appewtc/masterung/aectrips/MapsActivity.java
+++ app/src/main/java/appewtc/masterung/aectrips/MapsActivity.java
@@ -1,12 +1,14 @@
 package appewtc.masterung.aectrips;
 
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -16,6 +18,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private Double latCenterADouble, lngCenterADouble;
     private LatLng centerLatLng;
     private int countryAnInt;
+    private CountryTABLE objCountryTABLE;
+    private LatLng[] markerLatLngs;
+    private int[] iconCountryInts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,15 +34,69 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         //Get Lat&Lng to Center
         getCenterLatLng();
 
-        //Move To Center
-        // moveToCenter();
+        //Create LatLng for Marker
+        createLatLngForMarker();
 
 
     }   // onCreate
 
-    private void moveToCenter() {
+    private void createLatLngForMarker() {
 
-    }
+        objCountryTABLE = new CountryTABLE(this);
+
+        String[] strLat = objCountryTABLE.readAllCountry(4);
+        String[] strLng = objCountryTABLE.readAllCountry(5);
+        String[] strCountry = objCountryTABLE.readAllCountry(1);
+
+        iconCountryInts = new int[strCountry.length];
+        for (int i=0;i<strCountry.length;i++) {
+
+            Log.d("aec", "Country ==> " + strCountry[i]);
+
+
+
+            if (strCountry[i].equals("ไทย")) {
+                iconCountryInts[i] = R.drawable.th;
+            } else if (strCountry[i].equals("ลาว")) {
+                iconCountryInts[i] = R.drawable.la;
+            }else if (strCountry.equals("เวียดนาม")) {
+                iconCountryInts[i] = R.drawable.vn;
+            }else if (strCountry[i].equals("สิงคโปร์")) {
+                iconCountryInts[i] = R.drawable.sg;
+            }else if (strCountry[i].equals("ฟิลิปปินส์")) {
+                iconCountryInts[i] = R.drawable.ph;
+            }else if (strCountry[i].equals("เมียนม่า")) {
+                iconCountryInts[i] = R.drawable.mm;
+            }else if (strCountry[i].equals("อินโดนีเซีย")) {
+                iconCountryInts[i] = R.drawable.id;
+            }else if (strCountry[i].equals("กัมพูชา")) {
+                iconCountryInts[i] = R.drawable.cb;
+            }else if (strCountry.equals("บรูไน")) {
+                iconCountryInts[i] = R.drawable.bn;
+            } else if (strCountry[i].equals("มาเลเซีย")) {
+                iconCountryInts[i] = R.drawable.my;
+            } else {
+                iconCountryInts[i] = R.drawable.aec;
+                Log.d("aec", "bool = false");
+            }
+
+        }   //for
+
+        Double[] latDoubles = new Double[strLat.length];
+        Double[] lngDoubles = new Double[strLat.length];
+        markerLatLngs = new LatLng[strLat.length];
+
+        for (int i = 0; i < strLat.length; i++) {
+
+            latDoubles[i] = Double.parseDouble(strLat[i]);
+            lngDoubles[i] = Double.parseDouble(strLng[i]);
+
+            markerLatLngs[i] = new LatLng(latDoubles[i], lngDoubles[i]);
+
+        }   // for
+
+    }   // createLatLngForMarker
+
 
     private void getCenterLatLng() {
         latCenterADouble = getIntent().getDoubleExtra("Lat", 0);
@@ -53,10 +112,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         //Assign Center Map
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(centerLatLng, 6));
 
+
+        //Create All Marker
+        for (int i=0;i<markerLatLngs.length;i++) {
+
+            mMap.addMarker(new MarkerOptions().position(markerLatLngs[i]).
+            icon(BitmapDescriptorFactory.fromResource(iconCountryInts[i])));
+
+        }   // for
+
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(latCenterADouble, lngCenterADouble);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//        LatLng sydney = new LatLng(latCenterADouble, lngCenterADouble);
+//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
     }   // onMapReady
 
 }   // Main Class
