50300e9f2bd27d9e6bcfa24811623da6b43b5a0e
masterUNG <phrombutr@gmail.com>
2015-12-08 11:49:54 +0700

    Show Detail Finish

--- app/src/main/java/appewtc/masterung/aectrips/MarkerDetailActivity.java
+++ app/src/main/java/appewtc/masterung/aectrips/MarkerDetailActivity.java
@@ -11,7 +11,7 @@ import com.squareup.picasso.Picasso;
 public class MarkerDetailActivity extends AppCompatActivity {
 
     //Explicit
-    private TextView titleTextView;
+    private TextView titleTextView, detailTextView;
     private String titleString, descriptionString, latString, lngString, pictureString;
     private ImageView markerImageView;
 
@@ -66,12 +66,16 @@ public class MarkerDetailActivity extends AppCompatActivity {
                 .resize(250, 250)
                 .into(markerImageView);
 
+        //Show Detail
+        detailTextView.setText(descriptionString);
+
 
     }   // showView
 
     private void bindWidget() {
         titleTextView = (TextView) findViewById(R.id.textView14);
         markerImageView = (ImageView) findViewById(R.id.imageView12);
+        detailTextView = (TextView) findViewById(R.id.textView15);
     }
 
 }   // Main Class
